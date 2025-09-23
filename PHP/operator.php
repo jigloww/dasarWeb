9c1d01ad664d9a3bff05beab87663fce263f63e0
unknown <bima.rafi60@sma.belajar.id>
2025-09-23 10:49:47 +0700

    PHP-3.1

---
+++ PHP/operator.php
@@ -0,0 +1,20 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Variabel a: {$a} <br>";
+echo "Variabel b: {$b} <br>";
+echo "Hasil tambah: {$hasilTambah} <br>";
+echo "Hasil kurang: {$hasilKurang} <br>";
+echo "Hasil kali: {$hasilKali} <br>";
+echo "Hasil bagi: {$hasilBagi} <br>";
+echo "Sisa bagi: {$sisaBagi} <br>";
+echo "Pangkat: {$pangkat} <br>";
+?>
