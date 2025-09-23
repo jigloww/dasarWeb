c2cd902cb8948d86821d4af9a5de663dd48656aa
unknown <bima.rafi60@sma.belajar.id>
2025-09-23 21:55:36 +0700

    PHP-4.3

--- PHP/struktur_kontrol.php
+++ PHP/struktur_kontrol.php
@@ -23,4 +23,15 @@ while ($jarakSaatIni < $jarakTarget) {
 
 echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.<br>";
 
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
 ?>
