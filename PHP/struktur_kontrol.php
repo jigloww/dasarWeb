ad3ddb17477ea5e21ce1bbbe3501092e512c226d
unknown <bima.rafi60@sma.belajar.id>
2025-09-23 21:48:38 +0700

    PHP-4.2

--- PHP/struktur_kontrol.php
+++ PHP/struktur_kontrol.php
@@ -10,4 +10,17 @@ if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
 } else {
     echo "Nilai huruf: D <br>";
 }
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.<br>";
+
 ?>
