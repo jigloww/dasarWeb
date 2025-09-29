c223de10d302e11ad6ec23a74f5c2948b2577ca7
unknown <bima.rafi60@sma.belajar.id>
2025-09-29 23:19:29 +0700

    PHP2-1

---
+++ PHP-2/fungsi.php
@@ -0,0 +1,13 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan nama saya Tanggaq<br/>";
+    echo "Senang berkenalan dengan anda</br>";
+    echo "<br>";
+}
+
+perkenalan();
+perkenalan();
+
+?>
