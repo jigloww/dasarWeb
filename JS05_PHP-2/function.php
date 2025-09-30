849fa38aab63e58d4ec8e176b9b46ba1778e2285
unknown <bima.rafi60@sma.belajar.id>
2025-09-30 15:42:17 +0700

    JS05_PHP2-8

--- JS05_PHP-2/function.php
+++ JS05_PHP-2/function.php
@@ -3,6 +3,12 @@ function hitungUmur($thn_Lahir, $thn_Sekarang) {
     $umur = $thn_Sekarang - $thn_Lahir;
     return $umur;
 }
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $nama. ", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-echo "Umur saya adalah ". hitungUmur(2005, 2025) ." tahun";
+    echo "Saya berusia ". hitungUmur(2005, 2025) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+    perkenalan("Tanggaq");
 ?>
