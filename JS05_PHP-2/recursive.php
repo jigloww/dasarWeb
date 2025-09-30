c0cd7a83b765621a7948dc0a2dedabc3d2eb7685
unknown <bima.rafi60@sma.belajar.id>
2025-09-30 16:21:17 +0700

    JS05_PHP2-10

--- JS05_PHP-2/recursive.php
+++ JS05_PHP-2/recursive.php
@@ -1,9 +1,14 @@
 <?php
-function tampilkanHaloDunia() {
-    echo "Halo dunia! <br>";
-
-    tampilkanHaloDunia();
+for ($i=1; $i<=25; $i++) {
+    echo "Perulangan ke-{$i} <br>";
 }
+echo "<br>";
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
 
-tampilkanHaloDunia();
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
 ?>
