fa8b888b0bccc43a0c55498f2bb09d7886e3c297
unknown <bima.rafi60@sma.belajar.id>
2025-09-30 17:21:40 +0700

    JS05_PHP2-14

---
+++ JS05_PHP-2/string2.php
@@ -0,0 +1,12 @@
+<?php
+echo "Baris\nbaru <br>";   //10a
+echo 'Baris\nbaru <br>';   //10b
+echo "Halo\nDunia <br>";   //10c
+echo 'Halo\nDunia <br>';   //10d
+
+echo "<pre>Halo\tDunia!</pre>";  //10e  
+echo '<pre>Halo\tDunia!</pre>';  //10f
+
+echo "Katakanlah \"Tidak pada narkoba!\" <br>"; //10g 
+echo 'Katakanlah "Tidak pada narkoba!" <br>';   //10h 
+?>
