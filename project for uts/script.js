1206f8a1cbf6fe74c562bfae73ca5d840608d912
unknown <bima.rafi60@sma.belajar.id>
2025-10-07 22:53:07 +0700

    push

---
+++ project for uts/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("exploreBtn"); //jelajahi brand
+
+// ganti --> about.html
+button.addEventListener("click", function() {
+    window.location.href = "about.html";
+});
