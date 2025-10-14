467b3d2cca012512e90fcc1b3cf133a831129733
unknown <bima.rafi60@sma.belajar.id>
2025-10-14 22:35:22 +0700

    soal 5.2

--- PHP-Form/regex.php
+++ PHP-Form/regex.php
@@ -8,3 +8,15 @@ if (preg_match($pattern, $text)) {
     echo "Tidak ada huruf kecil!";
 }
 ?>
+<br>
+<br>
+<?php
+$pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit
+$text = 'There are 123 apples.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
+?>
