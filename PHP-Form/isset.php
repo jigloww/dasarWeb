a5041cc8a7922fc7b5788271090165151ed1209b
unknown <bima.rafi60@sma.belajar.id>
2025-10-14 20:26:36 +0700

    soal 1.2

--- PHP-Form/isset.php
+++ PHP-Form/isset.php
@@ -7,3 +7,14 @@ if (isset($umur) && $umur >= 18) {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
 }
 ?>
+<br>
+<br>
+<?php
+$data = array("nama" => "Jane", "usia" => 25);
+
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
+?>
