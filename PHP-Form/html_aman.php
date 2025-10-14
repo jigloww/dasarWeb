acf6df8ad44b24bd94f05c1e4159033c4d745f79
unknown <bima.rafi60@sma.belajar.id>
2025-10-14 22:19:23 +0700

    soal 4.2

--- PHP-Form/html_aman.php
+++ PHP-Form/html_aman.php
@@ -2,14 +2,30 @@
 if (isset($_POST['input'])) {
     $input = $_POST['input'];
     $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-    echo "Hasil input: " . $input;
+    echo "Hasil input: " . $input . "<br>";
 } else {
     echo "Silakan isi form di bawah ini : <br><br>";
+}
+
+// Memeriksa apakah input email dikirim terlebih dahulu
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+
+    // Memeriksa apakah input adalah email yang valid
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email valid: " . htmlspecialchars($email);
+    } else {
+        echo "Input bukan email yang valid!";
+    }
 }
 ?>
 
 <form method="post" action="">
-    <label for="input">Masukkan teks :</label>
-    <input type="text" name="input" id="input">
+    <label for="input">Masukkan teks :</label><br>
+    <input type="text" name="input" id="input"><br><br>
+
+    <label for="email">Masukkan email :</label><br>
+    <input type="text" name="email" id="email"><br><br>
+
     <input type="submit" value="Kirim">
 </form>
