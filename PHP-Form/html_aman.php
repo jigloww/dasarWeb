b88ddd0a7b0b2dd70245a68a62e5a8ed5e60dd0c
unknown <bima.rafi60@sma.belajar.id>
2025-10-14 21:51:20 +0700

    soal 4.1

--- PHP-Form/html_aman.php
+++ PHP-Form/html_aman.php
@@ -1,5 +1,15 @@
 <?php
-$input = $_POST['input'];
-$input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-echo "Hasil input: " . $input;
+if (isset($_POST['input'])) {
+    $input = $_POST['input'];
+    $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+    echo "Hasil input: " . $input;
+} else {
+    echo "Silakan isi form di bawah ini : <br><br>";
+}
 ?>
+
+<form method="post" action="">
+    <label for="input">Masukkan teks :</label>
+    <input type="text" name="input" id="input">
+    <input type="submit" value="Kirim">
+</form>
