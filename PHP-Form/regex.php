e0b3da16098c6fc5406c30767f19aeff86663186
unknown <bima.rafi60@sma.belajar.id>
2025-10-14 22:45:59 +0700

    soal 5.5

--- PHP-Form/regex.php
+++ PHP-Form/regex.php
@@ -42,3 +42,15 @@ if (preg_match($pattern, $text, $matches)) {
     echo "Tidak ada yang cocok!";
 }
 ?>
+<br>
+<br>
+<?php
+$pattern = '/go?d/'; // Cocokkan "gd" atau "god"
+$text = 'god is good.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
+?>
