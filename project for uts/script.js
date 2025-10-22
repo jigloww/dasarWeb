c5066a0c13cbe20b724c9c11ffddcc3bf8be9a6b
unknown <bima.rafi60@sma.belajar.id>
2025-10-22 18:36:05 +0700

    1

--- project for uts/script.js
+++ project for uts/script.js
@@ -1,16 +1,15 @@
-// Cek dulu: kalau ada tombol di halaman hero
+
 const button = document.querySelector("button");
 if (button) {
   button.addEventListener("click", function() {
-    window.location.href = "about.html"; // jika tombol memang ada
+    window.location.href = "about.html";
   });
 }
 
-// Cek juga link Jelajahi Brand
 const brandLink = document.getElementById("brand-link");
 if (brandLink) {
   brandLink.addEventListener("click", function(event) {
-    event.preventDefault(); // biar gak langsung ke link #
+    event.preventDefault();
     alert("Maaf, halaman web sedang dlm pengembangan");
   });
 }
