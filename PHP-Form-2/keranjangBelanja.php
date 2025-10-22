dc4f2df58f0115490a0d080654011b81141125c9
unknown <bima.rafi60@sma.belajar.id>
2025-10-22 22:59:43 +0700

    16, 17, 18, 19

---
+++ PHP-Form-2/keranjangBelanja.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Keranjang Belanja</title>
+</head>
+<body>
+    <h2>Keranjang Belanja</h2>
+
+    <?php
+    // Ambil data dari cookie
+    if (isset($_COOKIE['beliNovel']) && isset($_COOKIE['beliBuku'])) {
+        $beliNovel = $_COOKIE['beliNovel'];
+        $beliBuku = $_COOKIE['beliBuku'];
+
+        echo "Jumlah Novel: " . $beliNovel . "<br>";
+        echo "Jumlah Buku: " . $beliBuku;
+    } else {
+        echo "Keranjang belanja kosong!";
+    }
+    ?>
+</body>
+</html>
