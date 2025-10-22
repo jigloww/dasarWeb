5378e30f680df69041c2ca2bb427441858b80a37
unknown <bima.rafi60@sma.belajar.id>
2025-10-22 22:25:13 +0700

    12, 13, 14, 15

--- PHP-Form-2/form_upload_ajax.php
+++ PHP-Form-2/form_upload_ajax.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Multi Upload Gambar</title>
+    <link rel="stylesheet" href="upload.css">
+    <title>Unggah File Dokumen</title>
 </head>
 <body>
-    <h2>Unggah Gambar</h2>
+    <div class="upload-form-container">
+        <h2>Unggah File Dokumen</h2>
+        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
+            <div class="file-input-container">
+                <input type="file" name="file" id="file" class="file-input">
+                <label for="file" class="file-label">Pilih File</label>
+            </div>
+            <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
+        </form>
+        <div id="status" class="upload-status"></div>
+    </div>
 
-    <form action="upload_ajax.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="files[]" multiple accept=".jpg,.jpeg,.png,.gif">
-        <input type="submit" value="Unggah">
-    </form>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="upload.js"></script>
 </body>
 </html>
